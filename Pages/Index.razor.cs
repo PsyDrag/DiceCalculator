c76caecc3723edfb95b699235ceb4e98d91f2da6
PsyDrag <blaze94wall@gmail.com>
2021-02-14 15:03:23 -0600

    update main page

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -1,20 +1,35 @@
-﻿using DiceCalculator;
+﻿using System.Linq;
+using DiceCalculator;
 
 namespace WasmDiceCalculator.Pages
 {
     public partial class Index
     {
-        private int currentCount = 0;
-        private string diceRoll = "3d6";
-        private string minMax = "";
+        private string diceRollInput = "3d6";
+        private string diceRollOutput = "";
+        private string minMaxInput = "Min:1 Max:20";
+        private string minMaxOutput = "";
 
-        private void DoThingy()
+        private void CalculateDiceRoll()
         {
-            currentCount++;
-
-            var dr = Parser.ParseDiceRoll(diceRoll);
+            var dr = Parser.ParseDiceRoll(diceRollInput);
             var mm = Calculator.CalculateDiceRoll(dr);
-            minMax = $"Min:{mm.Min} Max:{mm.Max}";
+            diceRollOutput = $"Output:\nMin:{mm.Min} Avg:{mm.Avg} Max:{mm.Max}";
+        }
+
+        private void CalculateMinMax()
+        {
+            var mm = Parser.ParseMinMax(minMaxInput);
+            var dr = Calculator.CalculateMinMax(mm);
+
+            var die = dr.Dice.First();
+            minMaxOutput = $"Output:\n{die.TotalDiceAmount}d{die.DiceType}";
+
+            var mod = dr.Modifiers.First();
+            if (mod.Number != 0)
+            {
+                minMaxOutput += $" {mod.Operation} {mod.Number}";
+            }
         }
     }
 }
