359cda1eb935b9943c3e7d6fbca519d4ed71f707
PsyDrag <blaze94wall@gmail.com>
2021-02-14 14:25:32 -0600

    add help and examples pages

---
+++ DiceCalculator/Constants.cs
@@ -0,0 +1,50 @@
+﻿namespace DiceCalculator
+{
+    public static class Constants
+    {
+        public static readonly string DiceRollExamples =
+            "Dice Roll Examples:\n" +
+            "  1d4 + 7              standard roll plus modifier\n" +
+            "  3d6                  no modifier\n" +
+            "  d20                  straight up\n" +
+            "  d6 * 2               multiply\n" +
+            "  d20 + 3d6 + 5        multiple dice rolls\n" +
+            "  d6 - 3 * 2           multiple modifiers (calculations are made from left to right)\n" +
+            "  4kh3d6               standard ability roll\n" +
+            "  2kl1d20              with disadvantage\n" +
+            "  6kl5d4 + 3kh2d6 + 2  why would you want to do this fancy shit?\n" +
+            "  d20 + 6 ac 18        attack roll against AC 18\n" +
+            "  d20 + 4 dc 15        check against DC 15";
+
+        public static readonly string MinMaxExamples =
+            "Min-Max Examples:\n" +
+            "  min:10 max:48        2d20 + 8 is a possible result\n" +
+            "  min:6 max:17         d12 + 5 is a possible result\n" +
+            "  min:2 max:17         d16 + 1 is a possible result";
+        //  min:6 avg:11.5 max:17  d6 + 2d4 + 3 as a possible result
+        //  min:6 avg:15 max:17    d12 + 5 (min and max are prioritized over avg)
+
+        public static readonly string Help =
+            "Usage: [options]\n" +
+            "\n" +
+            "Options:\n" +
+            "  h|help                            Display help\n" +
+            "  e|exit                            Exit program\n" +
+            "  dr|diceroll [diceRollComponents]  You've been doing this for years so you ought to be able to\n" +
+            "  mm|minmax [minMaxComponents]      Must always include min and max\n" +
+            "  drex|drexamples                   Show examples of dice rolls\n" +
+            "  mmex|mmexamples                   Show examples of min-max values\n" +
+            "\n" +
+            "Dice Roll Components:\n" +
+            "  d                                 Do I really need to explain this one?\n" +
+            "  kh                                Keep highest x number of dice rolls (e.g. 4kh3)\n" +
+            "  kl                                Keep lowest x number of dice rolls (e.g. 2kl1)\n" +
+            "  ac                                Show pass/fail % against an AC\n" +
+            "  dc                                Show pass/fail % against a DC\n" +
+            "\n" +
+            "Min-Max Components:\n" +
+            "  min                               Minimum value\n" +
+            "  avg                               Average value\n" +
+            "  max                               Maximum value";
+    }
+}
--- DiceCalculator/Printer.cs
+++ DiceCalculator/Printer.cs
@@ -42,27 +42,7 @@ namespace DiceCalculator
         {
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine("Usage: [options]");
-            Console.WriteLine();
-            Console.WriteLine("Options:                            ");
-            Console.WriteLine("  h|help                            Display help");
-            Console.WriteLine("  e|exit                            Exit program");
-            Console.WriteLine("  dr|diceroll [diceRollComponents]  You've been doing this for years so you ought to be able to");
-            Console.WriteLine("  mm|minmax [minMaxComponents]      Must always include min and max");
-            Console.WriteLine("  drex|drexamples                   Show examples of dice rolls");
-            Console.WriteLine("  mmex|mmexamples                   Show examples of min-max values");
-            Console.WriteLine();
-            Console.WriteLine("Dice Roll Components:");
-            Console.WriteLine("  d                                 Do I really need to explain this one?");
-            Console.WriteLine("  kh                                Keep highest x number of dice rolls (e.g. 4kh3)");
-            Console.WriteLine("  kl                                Keep lowest x number of dice rolls (e.g. 2kl1)");
-            Console.WriteLine("  ac                                Show pass/fail % against an AC");
-            Console.WriteLine("  dc                                Show pass/fail % against a DC");
-            Console.WriteLine();
-            Console.WriteLine("Min-Max Components:");
-            Console.WriteLine("  min                               Minimum value");
-            Console.WriteLine("  avg                               Average value");
-            Console.WriteLine("  max                               Maximum value");
+            Console.WriteLine(Constants.Help);
             Console.WriteLine();
             Console.WriteLine();
         }
@@ -71,18 +51,7 @@ namespace DiceCalculator
         {
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine("Dice Roll Examples:");
-            Console.WriteLine("  1d4 + 7              standard roll plus modifier");
-            Console.WriteLine("  3d6                  no modifier");
-            Console.WriteLine("  d20                  straight up");
-            Console.WriteLine("  d6 * 2               multiply");
-            Console.WriteLine("  d20 + 3d6 + 5        multiple dice rolls");
-            Console.WriteLine("  d6 - 3 * 2           multiple modifiers (calculations are made from left to right)");
-            Console.WriteLine("  4kh3d6               standard ability roll");
-            Console.WriteLine("  2kl1d20              with disadvantage");
-            Console.WriteLine("  6kl5d4 + 3kh2d6 + 2  why would you want to do this fancy shit?");
-            Console.WriteLine("  d20 + 6 ac 18        attack roll against AC 18");
-            Console.WriteLine("  d20 + 4 dc 15        check against DC 15");
+            Console.WriteLine(Constants.DiceRollExamples);
             Console.WriteLine();
             Console.WriteLine();
         }
@@ -91,11 +60,7 @@ namespace DiceCalculator
         {
             Console.WriteLine();
             Console.WriteLine();
-            Console.WriteLine("Min-Max Examples:");
-            Console.WriteLine("  min:10 max:48          2d20 + 8 as a possible result");
-            Console.WriteLine("  min:6 max:17           d12 + 5 as a possible result");
-            //Console.WriteLine("  min:6 avg:11.5 max:17  d6 + 2d4 + 3 as a possible result");
-            //Console.WriteLine("  min:6 avg:15 max:17    d12 + 5 (min and max are prioritized over avg)");
+            Console.WriteLine(Constants.MinMaxExamples);
             Console.WriteLine();
             Console.WriteLine();
         }
