28b90336e525d88c2143a28a454d030075bcd83e
Isaac Stansell <isaac.stansell42@gmail.com>
2021-02-13 21:01:20 -0600

    Initial commit

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using DiceCalculator;
+
+namespace WasmDiceCalculator.Pages
+{
+    public partial class Index
+    {
+        private int currentCount = 0;
+        private string diceRoll = "3d6";
+        private string minMax = "";
+
+        private void DoThingy()
+        {
+            currentCount++;
+
+            var dr = Parser.ParseDiceRoll(diceRoll);
+            var mm = Calculator.CalculateDiceRoll(dr);
+            minMax = $"Min:{mm.Min} Max:{mm.Max}";
+        }
+    }
+}
