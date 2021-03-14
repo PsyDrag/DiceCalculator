d7288202728cec1380a61d7633015742b7092673
PsyDrag <blaze94wall@gmail.com>
2021-03-14 15:45:01 -0500

    add frequency and percentage to minmax result

--- DiceCalculator/MinMax.cs
+++ DiceCalculator/MinMax.cs
@@ -1,12 +1,28 @@
-﻿namespace DiceCalculator
+﻿using System.Collections.Generic;
+
+namespace DiceCalculator
 {
     public class MinMax
     {
         public MinMax(int min, float avg, int max)
             => (Min, Avg, Max) = (min, avg, max);
 
+        public MinMax(int min, float avg, int max, Dictionary<int, Calculations> calcs)
+            => (Min, Avg, Max, MinMaxCalcs) = (min, avg, max, calcs);
+
         public int Min { get; internal set; }
         public float Avg { get; internal set; }
         public int Max { get; internal set; }
+
+        public Dictionary<int, Calculations> MinMaxCalcs { get; internal set; }
+    }
+
+    public class Calculations
+    {
+        public Calculations(int freq, float pct)
+            => (Frequency, Percentage) = (freq, pct);
+
+        public int Frequency { get; internal set; }
+        public float Percentage { get; internal set; }
     }
 }
